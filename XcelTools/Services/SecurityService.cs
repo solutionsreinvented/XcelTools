3f4d2ca116d399bccc8f30acde2520d2e298ba7b
ReInvented <solutions.reinvented@gmail.com>
2025-03-04 14:48:04 +0530

    All files from separate assemblies are moved directly to the AddIn assembly

---
+++ XcelTools/Services/SecurityService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+using XcelTools.Xtractor.Interfaces;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace XcelTools.Services
+{
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class SecurityService : ISecurityService
+    {
+        #region Default Constructor
+
+        public SecurityService()
+        {
+            Password = "EngineeredProgramming";
+        }
+
+        #endregion
+
+        #region Private Properties
+
+        private string Password { get; set; }
+
+        #endregion
+
+        #region Public Functions
+
+        public void LockWorksheet(Excel.Worksheet worksheet)
+        {
+            try
+            {
+                worksheet.Protect(Password, UserInterfaceOnly: true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Following error encountered! {Environment.NewLine}{ex.Message}", "Lock Worksheet");
+            }
+        }
+
+        public void UnlockWorksheet(Excel.Worksheet worksheet)
+        {
+            worksheet.Unprotect(Password);
+        }
+
+        #endregion
+    }
+}
---
+++ XcelTools/Xtractor/Interfaces/ISectionsService.cs
@@ -0,0 +1,31 @@
+﻿using XcelTools.Xtractor.Models.Sections;
+
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace XcelTools.Xtractor.Interfaces
+{
+    [ComVisible(true)]
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+
+    public interface ISectionsService
+    {
+        [ComVisible(true)]
+        void PopulateSectionClassifications(string dbName, string shape, Excel.Range rngTarget);
+
+        [ComVisible(true)]
+        void PopulateSectionProfiles(string dbName, string shape, string classification, Excel.Range rngTarget, Excel.Range rngPopulation);
+
+        [ComVisible(true)]
+        IClassificationInfo GetClassification(ShapeInfo shapeInfo, string category);
+
+        [ComVisible(true)]
+        IShapeInfo GetShapeInfo(DatabaseInfo databaseInfo, string shape);
+
+        [ComVisible(true)]
+        IDatabaseInfo GetDatabaseInfo(string country);
+
+        //[ComVisible(true)]
+        //Catalogue GetSectionsCatalogue();
+    }
+}
